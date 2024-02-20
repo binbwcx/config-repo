6a1a6b29d0681c37ce42e6404d08b66bd629bc71
liguobin <liguobin0816@gmail>
2024-02-20 11:40:32 +0800

    Spring Cloud Config

--- config-client/src/main/java/org/example/Application.java
+++ config-client/src/main/java/org/example/Application.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //用来发现config-server服务
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).web(true).run(args);
